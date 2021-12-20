c5b92527f5b8a517d5f65e6d36fe53f5c87f13dd
Kirill Shevchenko <stranger.net3@gmail.com>
2021-12-20 05:15:07 +0400

    Fix

--- src/components/Tasks/TasksList.tsx
+++ src/components/Tasks/TasksList.tsx
@@ -11,24 +11,29 @@ interface Props {
 }
 
 export const TasksList: React.FC<Props> = observer(({ filter }) => {
-  const { tasks, completedTasks, activeTasks } = todoList;
-
-  const transition = useTransition(
+  const { tasks: allTasks, completedTasks, activeTasks } = todoList;
+  const tasks =
     filter === 'Active'
       ? activeTasks
       : filter === 'Completed'
       ? completedTasks
-      : tasks,
-    {
-      from: { opacity: 0, margin: '0px 0px' },
-      enter: { opacity: 1, margin: '5px 0px' },
-      leave: { opacity: 0, margin: '-20px 0px' },
-      config: { duration: 300 },
-    }
-  );
+      : allTasks;
+
+  const noTasksMessage = `No ${
+    filter !== 'All' ? filter.toLowerCase() : ''
+  } tasks ${filter !== 'Active' ? 'yet' : ''}`;
+
+  const transition = useTransition(tasks, {
+    from: { opacity: 0, margin: '0px 0px' },
+    enter: { opacity: 1, margin: '5px 0px' },
+    leave: { opacity: 0, margin: '-20px 0px' },
+    config: { duration: 300 },
+  });
 
   return (
     <ul className={s.tasksList}>
+      {tasks.length === 0 && <h3 className={s.noTasks}>{noTasksMessage}</h3>}
+
       {transition((style, task) =>
         task ? <Task {...task} style={style} /> : null
       )}
